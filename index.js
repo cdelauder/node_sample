ee6221344ff3703f649d8d1af299e3f67ba4406d
cdelauder <cdelauder@gmail.com>
2015-06-23 18:25:47 -0700

    add nodemon and write a rudimentary build script in package.json

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 
 var app = express();
+require('./controllers/route.js')(app);
 
 var server = app.listen(3000, function() {
-  console.log('listening at ', server.address().address, server.address().port)
+  console.log('listening at ', server.address().address, server.address().port);
 });
