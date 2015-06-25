ff1b564a9129ba1a754204318343c7b24658dad6
cdelauder <cdelauder@gmail.com>
2015-06-25 15:14:18 -0700

    added body-parser in order to be able to accept request data. also added an add route with a view that includes an input

--- controllers/route.js
+++ controllers/route.js
@@ -1,9 +1,37 @@
-
-
 module.exports = function(app) {
-  app.get('/', function(req, res) {
-    res.render('index', {
-      name: 'chris'    
+  var MongoClient = require('mongodb').MongoClient;
+  var url = 'mongodb://localhost:27017/node_sample';
+
+  MongoClient.connect(url, function(err, db) {
+    var names = db.collection('names');
+    console.log("Connected correctly to server");
+    app.get('/', function(req, res) {
+      var namesArray;
+      names.find({}).toArray(function(err, doc) {
+        if (err) {
+          console.log(err);
+        } else {
+          namesArray = doc;
+        }
+      })
+      res.render('index', {
+        names: namesArray    
+      });
     });
+    app.get('/add', function (req, res) {
+      res.render('add');
+    });
+    app.post('/add', function (req, res) {
+      console.log(req.body)
+      names.insert({
+        name: req.body.name
+      }, function (err, doc) {
+        if (err) {
+          res.send(err.message);
+        } else {
+          res.redirect('/');
+        }
+      })
+    })
   });
 };
--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var jade = require('jade');
 var path = require('path');
 
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', './public/views');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 require('./controllers/route.js')(app);
 
 var server = app.listen(3000, function() {
   console.log('listening at ', server.address().address, server.address().port);
 });
+
